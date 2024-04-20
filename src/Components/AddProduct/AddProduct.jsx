3cf95d07695a0e21c771d81bb226f6f11949e8b0
vvkreddy00 <vinaykumarreddyvelagala@gmail.com>
2024-04-21 01:57:34 +0530

    .

---
+++ src/Components/AddProduct/AddProduct.jsx
@@ -0,0 +1,135 @@
+import React, { useState } from "react";
+import "./AddProduct.css";
+import upload_area from "../Assets/upload_area.svg";
+
+const AddProduct = () => {
+  const [image, setImage] = useState(null);
+  const [productDetails, setProductDetails] = useState({
+    name: "",
+    image: "",
+    category: "women",
+    new_price: "",
+    old_price: "",
+  });
+
+  const AddProduct = async () => {
+    try {
+      let formData = new FormData();
+      formData.append("product", image);
+
+      // Upload image
+      const uploadResponse = await fetch("http://localhost:4000/upload", {
+        method: "POST",
+        body: formData,
+      });
+      const uploadData = await uploadResponse.json();
+
+      if (uploadData.success) {
+        // If image uploaded successfully, add product
+        const product = { ...productDetails, image: uploadData.image_url };
+        const addProductResponse = await fetch(
+          "http://localhost:4000/addproduct",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(product),
+          }
+        );
+        const addProductData = await addProductResponse.json();
+
+        if (addProductData.success) {
+          alert("Product Added");
+        } else {
+          alert("Failed to add product");
+        }
+      } else {
+        alert("Failed to upload image");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert("An error occurred. Please try again later.");
+    }
+  };
+
+  const changeHandler = (e) => {
+    setProductDetails({ ...productDetails, [e.target.name]: e.target.value });
+  };
+
+  const imageHandler = (e) => {
+    setImage(e.target.files[0]);
+  };
+
+  return (
+    <div className="addproduct">
+      <div className="addproduct-itemfield">
+        <p>Product title</p>
+        <input
+          type="text"
+          name="name"
+          value={productDetails.name}
+          onChange={changeHandler}
+          placeholder="Type here"
+        />
+      </div>
+      <div className="addproduct-price">
+        <div className="addproduct-itemfield">
+          <p>Price</p>
+          <input
+            type="text"
+            name="old_price"
+            value={productDetails.old_price}
+            onChange={changeHandler}
+            placeholder="Type here"
+          />
+        </div>
+        <div className="addproduct-itemfield">
+          <p>Offer Price</p>
+          <input
+            type="text"
+            name="new_price"
+            value={productDetails.new_price}
+            onChange={changeHandler}
+            placeholder="Type here"
+          />
+        </div>
+      </div>
+      <div className="addproduct-itemfield">
+        <p>Product category</p>
+        <select
+          value={productDetails.category}
+          name="category"
+          className="add-product-selector"
+          onChange={changeHandler}
+        >
+          <option value="women">Women</option>
+          <option value="men">Men</option>
+          <option value="kid">Kid</option>
+        </select>
+      </div>
+      <div className="addproduct-itemfield">
+        <p>Product image</p>
+        <label htmlFor="file-input">
+          <img
+            className="addproduct-thumbnail-img"
+            src={image ? URL.createObjectURL(image) : upload_area}
+            alt=""
+          />
+        </label>
+        <input
+          onChange={imageHandler}
+          type="file"
+          name="image"
+          id="file-input"
+          hidden
+        />
+      </div>
+      <button className="addproduct-btn" onClick={AddProduct}>
+        ADD
+      </button>
+    </div>
+  );
+};
+
+export default AddProduct;
